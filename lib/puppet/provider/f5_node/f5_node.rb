e1707d1fc3baba77db98c16632df0ef5d5bac134
Phil Pham <phil.pham@puppetlabs.com>
2014-04-10 16:10:40 -0700

    refactor node

--- lib/puppet/provider/f5_node/f5_node.rb
+++ lib/puppet/provider/f5_node/f5_node.rb
@@ -16,46 +16,33 @@ Puppet::Type.type(:f5_node).provide(:f5_node, :parent => Puppet::Provider::F5) d
 
   def self.instances
     Puppet.debug("Puppet::Provider::F5_Node: instances")
-    transport[wsdl].call(:get_list).body[:get_list_response][:return][:item].collect do |item|
+    Array(transport[wsdl].get(:get_list)).collect do |item|
       new(:name   => item,
           :ensure => :present
          )
     end
   end
 
-  def dynamic_ratio
-    message = { nodes: { item: resource[:name]}}
-    transport[wsdl].call(:get_dynamic_ratio, message: message).body[:get_dynamic_ratio_response][:return][:item]
-  end
+  methods = {
+    'dynamic_ratio'     => 'dynamic_ratios',
+    'ratio'             => 'ratios',
+    'connection_limit'  => 'limits'
+  }
 
-  def dynamic_ratio=(value)
-    message = { nodes: { item: resource[:name] }, dynamic_ratios: { item: resource[:dynamic_ratio] } }
-    transport[wsdl].call(:set_dynamic_ratio, message: message)
-  end
-
-  def ratio
-    message = { nodes: { item: resource[:name]}}
-    transport[wsdl].call(:get_ratio, message: message).body[:get_ratio_response][:return][:item]
-  end
-
-  def ratio=(value)
-    message = { nodes: { item: resource[:name] }, ratios: { item: resource[:ratio] } }
-    transport[wsdl].call(:set_ratio, message: message)
-  end
-
-  def connection_limit
-    message = { nodes: { item: resource[:name]}}
-    transport[wsdl].call(:get_connection_limit, message: message).body[:get_connection_limit_response][:return][:item]
-  end
-
-  def connection_limit=(value)
-    message = { nodes: { item: resource[:name]}, limits: { item: resource[:connection_limit]}}
-    transport[wsdl].call(:set_connection_limit, message: message)
-  end
+  methods.each do |method, arg|
+    define_method(method.to_sym) do
+      transport[wsdl].get("get_#{method}".to_sym, { nodes: { item: resource[:name] }})
+    end
+    define_method("#{method}=") do |value|
+      message = { nodes: { item: resource[:name] }, arg => { item: resource[method.to_sym] }}
+      transport[wsdl].call("set_#{method}".to_sym, message: message)
+    end
+  end 
 
   def session_enabled_state 
     message = { nodes: { item: resource[:name]}}
     value = transport[wsdl].call(:get_session_status, message: message).body[:get_session_status_response][:return][:item]
+
     case
     when value.match(/DISABLED$/)
       'STATE_DISABLED'
@@ -71,10 +58,6 @@ Puppet::Type.type(:f5_node).provide(:f5_node, :parent => Puppet::Provider::F5) d
     transport[wsdl].call(:set_session_enabled_state, message: message)
   end
 
-  def monitor_association
-    transport[wsdl].get_monitor_association(resource[:name])
-  end
-
   def create
     Puppet.debug("Puppet::Provider::F5_Node: creating F5 node #{resource[:name]}")
     # The F5 API isn't consistent, it accepts long instead of ULong64 so we set connection limits later.
@@ -85,10 +68,12 @@ Puppet::Type.type(:f5_node).provide(:f5_node, :parent => Puppet::Provider::F5) d
     }
     transport[wsdl].call(:create, message: message)
 
-    methods = [ 'connection_limit',
+    methods = [
+      'connection_limit',
       'dynamic_ratio',
       'ratio',
-      'session_enabled_state' ]
+      'session_enabled_state'
+     ]
 
     methods.each do |method|
       self.send("#{method}=", resource[method.to_sym]) if resource[method.to_sym]
@@ -101,9 +86,6 @@ Puppet::Type.type(:f5_node).provide(:f5_node, :parent => Puppet::Provider::F5) d
   end
 
   def exists?
-    response = transport[wsdl].call(:get_list)
-    if response.body[:get_list_response][:return][:item]
-      response.body[:get_list_response][:return][:item].include?(resource[:name])
-    end
+    transport[wsdl].get(:get_list).include?(resource[:name])
   end
 end
